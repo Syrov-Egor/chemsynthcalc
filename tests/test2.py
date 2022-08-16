37d8aef3abc5bdb9258d007356bf6921c771b918
Syrov-Egor <syrov_ev@mail.ru>
2022-08-16 11:54:16 +0300

    test2

--- tests/test2.py
+++ tests/test2.py
@@ -1,5 +1,5 @@
 from chemsynthcalc import ChemicalFormula
 from chemsynthcalc import ChemicalReaction
 
-reaction = ChemicalFormula("H2O")
-print(reaction.molar_mass)
+formula = ChemicalFormula("H2O")
+print(formula.molar_mass)
