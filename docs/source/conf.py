af0d366b5459263bdb49fbf0a91e9b83311ccaad
Syrov-Egor <syrov_ev@mail.ru>
2022-09-19 16:13:53 +0300

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,13 +19,11 @@ import sphinx_rtd_theme
 
 
 sys.path.append("./src")
+import chemsynthcalc
 
     # If extensions (or modules to document with autodoc) are in another directory,
     # add these directories to sys.path here. If the directory is relative to the
     # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-sys.path.insert(1, os.path.dirname(os.path.abspath("../")) + os.sep + "src")
 
 extensions = ['sphinx.ext.autodoc', 
 'sphinx.ext.autosummary', 
