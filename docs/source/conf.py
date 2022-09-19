a0f9aa08e96555bc7fe356a34a97df13d082c2c2
Syrov-Egor <syrov_ev@mail.ru>
2022-09-19 14:58:40 +0300

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,23 +44,6 @@ language = 'English'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinxdoc"
 html_static_path = ['_static']
 using_rtd_theme = True
-
-html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
