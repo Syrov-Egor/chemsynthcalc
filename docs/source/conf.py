c6d0b3f5c9fb91fc294ab55cbf34abbd28b3c73a
Syrov-Egor <syrov_ev@mail.ru>
2022-10-01 14:26:16 +0300

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,8 @@ import sys
 import os
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath("..//.."))
+sys.path.insert(0, os.path.abspath("src"))
+
 #sys.path.append("./src")
 
     # If extensions (or modules to document with autodoc) are in another directory,
