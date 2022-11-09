c6bb7197898116c1e89b84acc2a80820360475ab
Syrov-Egor <syrov_ev@mail.ru>
2022-11-09 16:55:16 +0300

    1.0.7

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -9,7 +9,7 @@
 project = 'chemsynthcalc'
 copyright = '2022, Egor Syrov'
 author = 'Egor Syrov'
-release = '1.0.6'
+release = '1.0.7'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
