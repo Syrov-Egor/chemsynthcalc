2c108feae49d999e81e08924c40f37e42c97d651
Syrov-Egor <syrov_ev@mail.ru>
2024-07-11 22:12:10 +0300

    strict PyRight type annotation for ChemicalFormulaParser

--- src/chemsynthcalc/formula_parser.py
+++ src/chemsynthcalc/formula_parser.py
@@ -5,83 +5,99 @@ class ChemicalFormulaParser:
     def __init__(self, formula: str) -> None:
         self.atom_regex: str = r"([A-Z][a-z]*)"
         self.coefficient_regex: str = r"((\d+(\.\d+)?)*)"
+        self.allowed_symbols: str = r"[^A-Za-z0-9.({[)}\]*·•]"
         self.atom_and_coefficient_regex: str = self.atom_regex + self.coefficient_regex
         self.opener_brackets: str = "({["
         self.closer_brackets: str = ")}]"
         self.adduct_symbols: str = "*·•"
-        self.allowed_symbols: str = r"[^A-Za-z0-9.({[)}\]*·•]"
 
         self.formula: str = formula
         self.atoms: set[str] = PeriodicTable().atoms
 
-    def _dictify(self, tuples: tuple[tuple]) -> dict:
-        """Transform tuples of tuples to a dict of atoms.
+    def _dictify(self, tuples: list[tuple[str, ...]]) -> dict[str, float]:
+        """Transform list of tuples to a dict of atoms.
 
         Arguments:
-            tuples (tuple): tuple of tuples of atoms
+            tuples (list): list of tuples of atoms
 
         Returns:
             dict: dictionary of atoms
         """
-        result: dict = dict()
-        for atom, n, m, k in tuples:
+        result: dict[str, float] = dict()
+        for atom, n, _, _ in tuples:
             try:
                 result[atom] += float(n or 1)
             except KeyError:
                 result[atom] = float(n or 1)
         return result
 
-    def _fuse(self, mol1: dict, mol2: dict, w: int = 1) -> dict:
-        """Fuse 2 dicts representing molecules. 
-        
+    def _fuse(
+        self, mol1: dict[str, float], mol2: dict[str, float], weight: float = 1.0
+    ) -> dict[str, float]:
+        """Fuse 2 dicts representing molecules.
+
         Arguments:
             mol1 (dict): dict of atoms 1
             mol2 (dict): dict of atoms 2
-            w (int): weight
+            weight (int): weight
 
         Returns:
-            dict: a new fuse dict
+            dict: a new fused dict
         """
 
-        fused_set: set = set(mol1) | set(mol2)
-        fused_dict: dict = {atom: (mol1.get(atom, 0) + mol2.get(atom, 0)) * w for atom in fused_set}
+        fused_set: set[str] = set(mol1) | set(mol2)
+        fused_dict: dict[str, float] = {
+            atom: (mol1.get(atom, 0) + mol2.get(atom, 0)) * weight for atom in fused_set
+        }
 
         return fused_dict
 
-    def _parse(self, formula: str) -> dict:
-        #TODO elif token in self.adduct_symbol
+    def _parse(self, formula: str) -> tuple[dict[str, float], int]:
+        # TODO if token in self.adduct_symbol
 
-        token_list: list = []
-        mol: dict = {}
+        token_list: list[str] = []
+        mol: dict[str, float] = {}
         i: int = 0
 
         while i < len(formula):
             token: str = formula[i]
 
             if token in self.closer_brackets:
-                coefficient_match: str = re.match(self.coefficient_regex, formula[i + 1 :]).group(0)
-                if coefficient_match != "":
-                    weight: float = float(coefficient_match)
-                    i += len(coefficient_match)
+                coefficient_match: re.Match[str] | None = re.match(
+                    self.coefficient_regex, formula[i + 1 :]
+                )
+                if coefficient_match != None:
+                    weight: float = float(coefficient_match.group(0))
+                    i += len(coefficient_match.group(0))
                 else:
                     weight = 1.0
-                submol: dict = self._dictify(re.findall(self.atom_and_coefficient_regex, "".join(token_list)))
+                submol: dict[str, float] = self._dictify(
+                    re.findall(self.atom_and_coefficient_regex, "".join(token_list))
+                )
+                return self._fuse(mol, submol, weight), i
 
             elif token in self.opener_brackets:
-                recursive_dive: tuple[dict, int] = self._parse(formula[i + 1 :])
-                submol: dict = recursive_dive[0]
+                recursive_dive: tuple[dict[str, float], int] = self._parse(
+                    formula[i + 1 :]
+                )
+                submol = recursive_dive[0]
                 lenght: int = recursive_dive[1]
                 mol = self._fuse(mol, submol)
                 i += lenght + 1
-            
+
             else:
                 token_list.append(token)
 
             i += 1
 
-        return token_list
+        extract_from_tokens: list[tuple[str, ...]] = re.findall(
+            self.atom_and_coefficient_regex, "".join(token_list)
+        )
+        fused_dict: dict[str, float] = self._fuse(
+            mol, self._dictify(extract_from_tokens)
+        )
+        return fused_dict, i
 
-    def parse_formula(self) -> dict:
-        output: dict = {}
-        output = self._parse(self.formula)
+    def parse_formula(self) -> dict[str, float]:
+        output: dict[str, float] = self._parse(self.formula)[0]
         return output
