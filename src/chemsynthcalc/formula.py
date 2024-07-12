c6be9b36972b0a119485fcf4589dbd7f1929fcb8
Syrov-Egor <syrov_ev@mail.ru>
2024-07-12 16:51:52 +0300

    Added ChemicalFormula as a separate abstract base class

---
+++ src/chemsynthcalc/formula.py
@@ -0,0 +1,12 @@
+class ChemicalFormula():
+
+    def __init__(self, formula: str) -> None:
+        self.atom_regex: str = r"([A-Z][a-z]*)"
+        self.coefficient_regex: str = r"((\d+(\.\d+)?)*)"
+        self.allowed_symbols: str = r"[^A-Za-z0-9.({[)}\]*·•]"
+        self.atom_and_coefficient_regex: str = self.atom_regex + self.coefficient_regex
+        self.opener_brackets: str = "({["
+        self.closer_brackets: str = ")}]"
+        self.adduct_symbols: str = "*·•"
+
+        self.formula: str = formula
