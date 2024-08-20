5789890ef7e659da96978bfa506f8b15589724f5
Syrov-Egor <syrov_ev@mail.ru>
2024-08-20 18:52:30 +0300

    Tests formatted

--- tests/test_formula_validator.py
+++ tests/test_formula_validator.py
@@ -8,30 +8,36 @@ from chemsynthcalc.chem_errors import (
     BracketsNotPaired,
 )
 
+
 def test_empty_formula():
     formula = ""
     with pytest.raises(EmptyFormula):
         FormulaValidator(formula).validate_formula()
+
 
 def test_one_invalid_characher():
     formula = "猫H2O"
     with pytest.raises(InvalidCharacter):
         FormulaValidator(formula).validate_formula()
 
+
 def test_invalid_atoms():
     formula = "Hu2O"
     with pytest.raises(NoSuchAtom):
         FormulaValidator(formula).validate_formula()
+
 
 def test_unbalanced_brackets():
     formula = "K2Mg2(SO4)3)"
     with pytest.raises(BracketsNotPaired):
         FormulaValidator(formula).validate_formula()
 
+
 def test_more_than_one_adduct():
     formula = "K2Mg2(SO4)3*2H2O*HCl"
     with pytest.raises(MoreThanOneAdduct):
-        FormulaValidator(formula).validate_formula() 
+        FormulaValidator(formula).validate_formula()
+
 
 def test_all_valid():
     formula = "K2Mg2(SO4)3*2H2O"
