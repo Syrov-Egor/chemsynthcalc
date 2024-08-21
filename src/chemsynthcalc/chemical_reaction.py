6bf1e1fc9c092c818aba0a0c92e4945cb50c79eb
Syrov-Egor <syrov_ev@mail.ru>
2024-08-21 18:19:14 +0300

    Reaction decomposition added

--- src/chemsynthcalc/chemical_reaction.py
+++ src/chemsynthcalc/chemical_reaction.py
@@ -1,6 +1,24 @@
+from functools import lru_cache
+
 from .reaction_validator import ReactionValidator
+from .reaction_decomposer import ReactionDecomposer
+from .chemical_formula import ChemicalFormula
 
 class ChemicalReaction:
-    def __init__(self, reaction: str) -> None:
+    def __init__(self, reaction: str = "", precision: int = 8) -> None:
         if ReactionValidator(reaction).validate_reaction():
             self.reaction = reaction
+
+        if precision > 0:
+            self.precision: int = precision
+        else:
+            raise ValueError("precision <= 0")
+
+        self.decomposed_reaction = ReactionDecomposer(self.reaction)
+        self.chemformula_objs: list[ChemicalFormula] = [ChemicalFormula(formula, self.precision) for formula in self.decomposed_reaction.compounds]
+
+    @property
+    @lru_cache
+    def parsed_formulas(self) -> list[dict[str, float]]:
+        return [compound.parsed_formula for compound in self.chemformula_objs]
+        
--- src/chemsynthcalc/reaction_decomposer.py
+++ src/chemsynthcalc/reaction_decomposer.py
@@ -1,4 +1,29 @@
 from .reaction import Reaction
-from .reaction_validator import ReactionValidator
 
 class ReactionDecomposer(Reaction):
+    def __init__(self, reaction: str) -> None:
+        super().__init__(reaction)
+
+        self.separator: str = self.extract_separator()
+
+        self._initial_reactants: list[str] = self.reaction.split(self.separator)[0].split(self.reactant_separator)
+        self._initial_products: list[str] = self.reaction.split(self.separator)[1].split(self.reactant_separator)
+        self._splitted_compounds: list[tuple[float, str]] = [self.split_coefficient_from_formula(formula) for formula in self._initial_reactants + self._initial_products]
+        
+        self.initial_coefficients: list[float] = [atom[0] for atom in self._splitted_compounds]
+        self.compounds: list[str] = [atom[1] for atom in self._splitted_compounds]
+        self.reactants: list[str] = self.compounds[:len(self._initial_reactants)]
+        self.products: list[str] = self.compounds[len(self._initial_reactants):]
+
+    def split_coefficient_from_formula(self, formula: str) -> tuple[float, str]:
+        if not formula[0].isdigit():
+            return 1.0, formula
+        else:
+            coef: list[str] = []
+            i: int = 0
+            for i, symbol in enumerate(formula):
+                if symbol.isdigit() or symbol == ".":
+                    coef.append(symbol)
+                else:
+                    break
+            return float("".join(coef)), formula[i:]
