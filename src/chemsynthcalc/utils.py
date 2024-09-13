81b2c18d7aa3cc4f8982f94e162e39f262da3d89
Syrov-Egor <syrov_ev@mail.ru>
2024-09-13 16:31:05 +0300

    Typing for utils

--- src/chemsynthcalc/utils.py
+++ src/chemsynthcalc/utils.py
@@ -12,37 +12,13 @@ def to_integer(coefficients: list[float | int]) -> list[float | int]:
     return [int(i) if i.is_integer() else i for i in coefficients]
 
 
-def find_lcm(int_list: list) -> int:
-    """Finds least common multiple of the list of integers.
-
-    Arguments:
-        int_list (list): list of integers
-
-    Returns:
-        int: Least common multiple
-
-    Example:
-        >>> chemsynthcalc.chemutils.find_lcm([10,20,30]
-        60
-    """
+def find_lcm(int_list: list[int]) -> int:
     lcm = 1
     for i in int_list:
         lcm = lcm * i // gcd(lcm, i)
     return lcm
 
 
-def find_gcd(int_list: list) -> int:
-    """Finds the greatest common divider of the list of integers.
-
-    Arguments:
-        int_list (list): list of integers
-
-    Returns:
-        int: greatest common divider
-
-    Example:
-        >>> chemsynthcalc.chemutils.find_gcd([10,20,30]
-        10
-    """
+def find_gcd(int_list: list[int]) -> int:
     x = reduce(gcd, int_list)
     return x
