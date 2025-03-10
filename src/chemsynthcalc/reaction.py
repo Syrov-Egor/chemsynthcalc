171bb36f220e4996573c085308c921c31cd4d009
Syrov-Egor <syrov_ev@mail.ru>
2025-03-10 09:51:36 +0300

    docs for Reaction class

--- src/chemsynthcalc/reaction.py
+++ src/chemsynthcalc/reaction.py
@@ -1,5 +1,18 @@
 class Reaction:
-    """ """
+    """
+    A base class for
+    [ReactionDecomposer][chemsynthcalc.reaction_decomposer.ReactionDecomposer] and
+    [ReactionValidator][chemsynthcalc.reaction_validator.ReactionValidator]
+    containing regexes and symbols.
+
+    Parameters:
+        reaction (str): Reaction string
+
+    Attributes:
+        allowed_symbols (str): Regex of all symbols allowed in a reaction string
+        possible_reaction_separators (list[str]): List of all allowed reaction separators (left and right part separator)
+        reactant_separator (str): Only one possible reactant separator ("+")
+    """
 
     def __init__(self, reaction: str) -> None:
         self.allowed_symbols: str = r"[^a-zA-Z0-9.({[)}\]*·•=<\->→⇄+ ]"
@@ -18,6 +31,13 @@ class Reaction:
         self.reaction = reaction
 
     def extract_separator(self) -> str:
+        """
+        Extract one of possible reaction separator from
+        the reaction string.
+
+        Returns:
+            Separator string if separator is found, empty string if not
+        """
         for separator in self.possible_reaction_separators:
             if self.reaction.find(separator) != -1:
                 if (
